366eb00047e05d1ce11e4c5e28134401fd131220
Damankin6 <69785211+Damankin6@users.noreply.github.com>
2026-03-12 15:58:59 -0400

    Create clock.js

---
+++ clock.js
@@ -0,0 +1,18 @@
+function updateClock() {
+    const now = new Date();
+
+    const options = {
+        timeZone: "America/New_York",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: true
+    };
+
+    const timeString = new Intl.DateTimeFormat("en-US", options).format(now);
+
+    document.getElementById("clock").textContent = "Streamers Local Time: " + timeString;
+}
+
+setInterval(updateClock, 1000);
+updateClock();
